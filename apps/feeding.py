1120297e94b2073428d08b05ab1d67e1273a86e7
buffalorwa-admin <111390607+buffalorwa-admin@users.noreply.github.com>
2022-08-23 15:22:37 -0500

    Add files via upload

--- apps/feeding.py
+++ apps/feeding.py
@@ -8,23 +8,101 @@ from folium.plugins import BeautifyIcon, MarkerCluster, Fullscreen
 import streamlit.components.v1 as components
 import geopandas as gpd
 
-def load_xy_to_cluster(shpname,icon_name='home',popup_cols=[],
+
+
+
+popup_dict = {'source':"Data Source",'inside':"Miles from BNRW",
+              'last_active':"Last year active",'gps':'GPS'}
+
+@st.cache  
+def popupHTML(row, popup_dict=popup_dict,col1width=50):
+    '''Create custom HTML for popup.
+
+    Parameters
+    ----------
+    row : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    html : TYPE
+        DESCRIPTION.
+        
+    After: https://towardsdatascience.com/folium-map-how-to-create-a-table-style-pop-up-with-html-code-76903706b88a
+           and https://github.com/Gromopo/GroMoPo
+
+    '''
+    html = """<!DOCTYPE html>
+            <html>
+            
+            <table>
+            <tbody>
+            """
+    
+    for key, value in popup_dict.items():
+        # Loop through entries for popup with specific formatting
+        
+        if row[key] is None:
+            second_col_val= 'N/A'
+        else:
+            second_col_val = row[key]
+        
+        if 'link' in value: # format for clickable link
+            html += """<tr>
+                       <td><span style="text-align: left;width: {0}px; color: #293191; overflow-wrap: break-word;"><b>{1}</b></span></td>
+                       <td><span style="text-align: right;overflow-wrap: break-word;>""".format(col1width,value) # Attribute name
+                       
+            # if second_col_val != 'N/A' and second_col_val is not None:
+                
+            links = second_col_val.split('|')
+            links.insert(0,"") # for some reason need a dummy entry, as first entry doesn't show up as link, only as text.
+            
+                
+            # link_html = """ """.join(["""<a href="{0}" target="_blank"> {0}</a><br>""".format(link) for link in links])
+            link_html = """ """
+            for i,link in enumerate(links):
+                if i == 0:
+                    link_html += """<a href="{0}" target="_blank">{0}</a>""".format(link)
+                elif link == 'N/A':
+                    link_html += """{}<br>""".format(link)
+                else:
+                    link_html += """<a href="{0}" target="_blank">{0}</a><br>""".format(link)
+            
+            html += link_html
+            # else:
+            #     html += """{0}<br>""".format(second_col_val)
+                
+            html += """</span></td></tr>"""
+            # html += """</td></td></td></tr>"""
+
+
+        else:
+            html += """<tr>
+                       <td style="text-align: left;"><span style="width: {0}px;color: #000000;overflow-wrap: break-word;"><b>{1}</b></span></td>
+                       <td style="text-align: right; overflow-wrap: break-word;">{2}</td></tr>""".format(col1width,value,second_col_val)
+                       
+    
+    html += """</tbody>
+               </table>                    
+               </html>"""
+    
+    return html
+
+def load_xy_to_cluster(shp_df,icon_name='home',popup_cols=[],
                        include_latlon=True,min_width=100,max_width=200,):
-    shp_df = gpd.read_file(shp_path)
-    lon = shp_df.geometry.x.values
-    lat = shp_df.geometry.y.values
     
     m_cluster = MarkerCluster(name="Poultry Houses",control=True)
     
     # Only works with v4, https://fontawesome.com/v4/icons/
     # in v5, there is 'turkey'
-    for ilon,ilat in zip(lon,lat):
+    # for ilon,ilat in zip(lon,lat):
+    for irow,row_df in shp_df.iterrows():
         
-        html = ""
-        if include_latlon: # add latitude and longitude
-            html = html + "<b>" + 'GPS' + "</b>" + ": " + "{0:3.4f},{1:3.4f}".format(ilat,ilon) + "<br>"
-        for p in popup_cols:
-            html = html + "<b>" + p + "</b>" + ": " + str(row[p]) + "<br>"
+        html = row_df['popup_html']
+        # if include_latlon: # add latitude and longitude
+        #     html = html + "<b>" + 'GPS' + "</b>" + ": " + "{0:3.4f},{1:3.4f}".format(row_df.geometry.y,row_df.geometry.x) + "<br>"
+        # for p in popup_cols:
+        #     html = html + "<b>" + p + "</b>" + ": " + str(row[p]) + "<br>"
         
         
         if len(html)>0:
@@ -32,12 +110,18 @@ def load_xy_to_cluster(shpname,icon_name='home',popup_cols=[],
         else:
             mark_popup = None
         
-        folium.Marker([ilat,ilon],
+        if row_df['active'] == 1:
+            color = 'black'
+        else:
+            color = 'grey'
+        
+        
+        folium.Marker([row_df.geometry.y,row_df.geometry.x],
                popup=mark_popup,
                icon=BeautifyIcon(icon=icon_name,
                                  icon_shape='circle', 
                                  border_color='transparent', 
-                                 inner_icon_style= 'color:black;font-size:30px',
+                                 inner_icon_style= 'color:{};font-size:30px'.format(color),
                                  # border_width=2,
                                  background_color='transparent')).add_to(m_cluster)
     return m_cluster
@@ -47,10 +131,17 @@ def load_xy_to_cluster(shpname,icon_name='home',popup_cols=[],
 # else:
 main_path = Path(".")
     
-shp_path = str(main_path.absolute().joinpath('data', 'bnr_chicken_house.geojson'))
+shp_path = str(main_path.absolute().joinpath('data', 'bnr_chicken_house+2mile.geojson'))
+shp_df = gpd.read_file(shp_path)
+shp_df.loc[shp_df['last_active'].isna(),'last_active'] = 2022
+shp_df['last_active'] = gpd.pd.to_numeric(shp_df['last_active'],downcast="integer")
+shp_df['gps'] = shp_df.apply(lambda row: "{0:3.4f},{1:3.4f}".format(row.geometry.xy[1][0],row.geometry.xy[0][0]),axis=1)
 
-# shp_path = r"data\bnr_chicken_house.geojson"
-m_cluster = load_xy_to_cluster(shp_path)
+
+shp_df['popup_html'] = shp_df.apply(popupHTML,axis=1)
+
+
+m_cluster = load_xy_to_cluster(shp_df)
 
 ws_path = str(main_path.absolute().joinpath('data', 'bnr_ws_hu8.geojson'))
 ws_style = lambda x:{'lineColor':'#F0F8FF',
@@ -148,11 +239,14 @@ def app():
         st.markdown('''
                     # Poultry Operations \n
                     
-                    **50ish** houses within the BNR watershed \n
-                    **50ish** additional houses within 2 miles of the BNR watershed \n
-                    **xyz** estimated active houses \n
+                    **70** houses within the BNR watershed \n
+                    **65** additional houses within 2 miles of the BNR watershed \n
+                    **98** estimated active houses <img src="https://raw.githubusercontent.com/FortAwesome/Font-Awesome/6.x/svgs/solid/house.svg" width="20" height="20"> within 2 miles of BNR watershed \n
                     ** xzy ft^2** roof area ~= **n chickens** and \n
                     **xzy lb/yr solid waste** \n
+                    
+                    **Black** houses are active, <span style="color:grey">**Grey**</span> inactive
+                    
                     ---
                     # Hog operations \n
                     
@@ -164,5 +258,16 @@ def app():
     with st.expander("Additional information"):
         st.markdown(about_text)
         st.write("""
-                 The calculations were ...
+                 The calculations for the solid waste production were based on 
+                 the methods developed for the WaterKeeper Alliance and EWG
+                 ["Fields of Filth" interactive map](https://www.ewg.org/interactive-maps/2020-fields-of-filth/map/).
+                 This methodology is further described  at https://www.ewg.org/research/exposing-fields-filth-north-carolina.
+                 
+                 
+                 
+                 
+                 
+                 # References \n
+                 AHTD (Arkansas Highway and Transportation Department) Chicken Houses, August 29, 2006: https://gis.arkansas.gov/product/chicken-house-point/
+                 
                  """)
